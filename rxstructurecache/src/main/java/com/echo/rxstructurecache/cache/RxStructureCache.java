c4541d34749409206e63bf68c3bdd53bb47e6bdc
jiangecho <jiangyingji1@163.com>
2016-04-04 17:13:40 +0800

    add comment

--- rxstructurecache/src/main/java/com/echo/rxstructurecache/cache/RxStructureCache.java
+++ rxstructurecache/src/main/java/com/echo/rxstructurecache/cache/RxStructureCache.java
@@ -18,6 +18,8 @@ import rx.functions.Action1;
 import rx.functions.Func1;
 
 /**
+ * cache your structure data, such as news, rank list and so on
+ * <p/>
  * Created by jiangecho on 16/4/3.
  */
 public class RxStructureCache<T extends CacheAble> {
@@ -32,6 +34,14 @@ public class RxStructureCache<T extends CacheAble> {
 
     }
 
+    /**
+     * get data firstly from cache, then continue to get data from loader.
+     * onNext maybe called twice, one with cached data, and the other one with remote data
+     *
+     * @param clazz  your model class
+     * @param loader remote loader
+     * @return a list of entity, latest update entity first
+     */
     public Observable<List<T>> getFromCacheThenLoader(Class<T> clazz, Observable<List<T>> loader) {
         Query query = cacheDao.queryBuilder()
                 .where(CacheDao.Properties.Type.eq(clazz.getName()))
@@ -60,6 +70,13 @@ public class RxStructureCache<T extends CacheAble> {
                 }));
     }
 
+    /**
+     * get data directly from loader, and before emit the data, cache it.
+     * onNext would be called only once with the remote data
+     *
+     * @param loader custom loader
+     * @return
+     */
     public Observable<List<T>> getFromLoaderAndCache(Observable<List<T>> loader) {
         return loader.filter(new Func1<List<T>, Boolean>() {
             @Override
@@ -75,6 +92,11 @@ public class RxStructureCache<T extends CacheAble> {
         });
     }
 
+    /**
+     * cache remote data to db
+     *
+     * @param datas data to be cached
+     */
     private void cache(List<T> datas) {
         if (datas == null || datas.size() == 0) {
             return;
@@ -106,6 +128,13 @@ public class RxStructureCache<T extends CacheAble> {
 
     }
 
+    /**
+     * get one entity from cache
+     *
+     * @param clazz   model class
+     * @param cacheId cache id
+     * @return
+     */
     public T get(Class<T> clazz, long cacheId) {
         Query query = cacheDao.queryBuilder()
                 .where(CacheDao.Properties.CacheId.eq(cacheId))
@@ -116,15 +145,33 @@ public class RxStructureCache<T extends CacheAble> {
         return fromCache(clazz, cache);
     }
 
+    /**
+     * get one entity from cache
+     *
+     * @param clazz   model class
+     * @param cacheId
+     * @return an observable which will emit the cached entity
+     */
     public Observable<T> getOb(Class<T> clazz, long cacheId) {
         return Observable.just(get(clazz, cacheId));
     }
 
+    /**
+     * update entity in cache, after this operation the id of the cached entity will be update,
+     * it would affect the rank position
+     *
+     * @param record
+     */
     public void update(T record) {
         Cache cache = toCache(record);
         cacheDao.insertOrReplaceInTx(cache);
     }
 
+    /**
+     * delete one entity from cache
+     *
+     * @param record entity to be deleted
+     */
     public void delete(T record) {
         DeleteQuery query = cacheDao.queryBuilder()
                 .where(CacheDao.Properties.CacheId.eq(record.getCacheId()))
